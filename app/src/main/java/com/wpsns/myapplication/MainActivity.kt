fe0f9bb675c1dcc0ba7597aef766c0c8982f5a05
yuknight <278671223@qq.com>
2022-08-06 22:21:00 +0800

    创建并提交分支1

--- app/src/main/java/com/wpsns/myapplication/MainActivity.kt
+++ app/src/main/java/com/wpsns/myapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package com.wpsns.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("TAG", "onCreate: 创建分支1")
     }
 }
