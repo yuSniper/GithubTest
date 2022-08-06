3098344228a7d2cd6ce06496a91da07cbce5c1b5
yuknight <278671223@qq.com>
2022-08-06 22:51:44 +0800

    用户1分支一修改你内容3

--- app/src/main/java/com/wpsns/myapplication/MainActivity.kt
+++ app/src/main/java/com/wpsns/myapplication/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
 
         Log.i("TAG", "onCreate:用户一， 创建分支1")
         Log.i("TAG", "onCreate:用户一， 修改内容2")
+        Log.i("TAG", "onCreate:用户一， 分支一修改内容3")
+
 
     }
 }
